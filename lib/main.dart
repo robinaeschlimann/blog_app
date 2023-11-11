f90dd41a90995595ba573187bc6e22492e4c6c7c
robin.aeschlimann <robin.aeschlimann@hftm.ch>
2023-11-11 09:52:00 +0100

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+import 'blog_page.dart';
+import 'settings_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+
+    return MaterialApp(
+      home: DefaultTabController(
+        length: 2,
+        child: Scaffold(
+          appBar: AppBar(
+            backgroundColor: const Color(0xFFC5264E),
+            title: const Text("Blog App"),
+          ),
+          bottomNavigationBar: menu(),
+          body: const TabBarView(
+            children: [
+              BlogPage(),
+              SettingsPage(),
+            ]
+          ),
+        )
+      )
+    );
+  }
+}
+
+Widget menu() {
+  return Container(
+    color: Color(0xFFC5264E),
+    child: const TabBar(
+      indicatorColor: Colors.white,
+      tabs: [
+        Tab(
+          text: "Blogs",
+          icon: Icon(Icons.abc_outlined),
+        ),
+        Tab(
+          text: "Settings",
+          icon: Icon(Icons.settings),
+        ),
+      ],
+    ),
+  );
+}
