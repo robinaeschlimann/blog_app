b14f9a57763d6796a82f5519277937ac7596d640
robin.aeschlimann <robin.aeschlimann@hftm.ch>
2023-11-12 17:48:49 +0100

    feat: add action icons in AppBar

--- lib/main.dart
+++ lib/main.dart
@@ -20,16 +20,14 @@ class BlogApp extends StatelessWidget {
           primaryColor: const Color(0xFFC5264E),
           useMaterial3: true,
         ),
-        home: const MyHomePage(title: 'Flutter Demo Home Page'),
+        home: const MyHomePage(),
       )
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
+  const MyHomePage({super.key});
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
@@ -47,6 +45,19 @@ class _MyHomePageState extends State<MyHomePage> {
           appBar: AppBar(
             backgroundColor: const Color(0xFFC5264E),
             title: const Text("Blog App"),
+            actions: [
+              IconButton(
+                onPressed: () {
+
+                },
+                icon: const Icon(Icons.search),
+              ),
+              IconButton(
+                  onPressed: () {
+
+                  },
+                  icon: const Icon(Icons.person)),
+            ],
           ),
           bottomNavigationBar: menu(),
           body: const TabBarView(
