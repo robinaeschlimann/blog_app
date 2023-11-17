e1f8bde96459eda48759de099b3fa97828d06d95
robin.aeschlimann <robin.aeschlimann@hftm.ch>
2023-11-17 13:43:56 +0100

    feat: settings page

--- lib/settings_page.dart
+++ lib/settings_page.dart
@@ -12,9 +12,30 @@ class _SettingsPageState extends State<SettingsPage> {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return const Scaffold(
-      body: Center(
-        child: Text("Settings Page"),
+    return Scaffold(
+      body: ListView(
+        children: const [
+          ListTile(
+            title: Text("Profile"),
+            leading: Icon(Icons.person),
+            trailing: Icon(Icons.arrow_forward_ios)
+          ),
+          ListTile(
+            title: Text("Favorites"),
+            leading: Icon(Icons.favorite),
+            trailing: Icon(Icons.arrow_forward_ios)
+          ),
+          ListTile(
+              title: Text("Language"),
+              leading: Icon(Icons.language),
+              trailing: Icon(Icons.arrow_forward_ios)
+          ),
+          ListTile(
+              title: Text("Setting X"),
+              leading: Icon(Icons.settings),
+              trailing: Icon(Icons.arrow_forward_ios)
+          )
+        ]
       ),
     );
   }
