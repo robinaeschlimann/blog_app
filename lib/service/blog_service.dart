bf3f3d898e260705bbaada25da0d77a1bd48fe5a
robin.aeschlimann <robin.aeschlimann@hftm.ch>
2024-02-13 17:47:19 +0100

    feat: implement deletion of a blog

--- lib/service/blog_service.dart
+++ lib/service/blog_service.dart
@@ -48,9 +48,10 @@ class BlogService {
 
   Future<bool> addBlog(Blog? blog) async {
     if( blog == null )
-      {
-        return false;
-      }
+    {
+      return false;
+    }
+
     try {
       final response = await http.post(Uri.parse('https://cloud.appwrite.io/v1/databases/blog-db/collections/blogs/documents'), headers: {
         'X-Appwrite-Project': '6568509f75ac404ff6ae',
@@ -72,6 +73,27 @@ class BlogService {
         // then throw an exception.
         logger.e("Failed to add blog, status code: ${response.statusCode}, reason: ${response.reasonPhrase}");
         throw Exception('Failed to add blog');
+      }
+    }
+    catch (e) {
+      return false;
+    }
+  }
+
+  deleteBlog( String blogId ) async {
+    try {
+      final response = await http.delete(Uri.parse('https://cloud.appwrite.io/v1/databases/blog-db/collections/blogs/documents/$blogId'), headers: {
+        'X-Appwrite-Project': '6568509f75ac404ff6ae',
+        'X-Appwrite-Key': 'ac0f362d0cf82fe3d138195e142c0a87a88cee4e2c48821192fb307e1a1c74ee694246f90082b4441aa98a2edaddead28ed6d18cf08c4de0df90dcaeeb53d14f14fb9eeb2edec6708c9553434f1d8df8f8acbfbefd35cccb70f2ab0f9a334dfd979b6052f6e8b8610d57465cbe8d71a7f65e8d48aede789eef6b976b1fe9b2e2'
+      });
+
+      if(response.statusCode == 204) {
+        return true;
+      } else {
+        // If the server did not return a 204 OK response,
+        // then throw an exception.
+        logger.e("Failed to delete blog, status code: ${response.statusCode}, reason: ${response.reasonPhrase}");
+        throw Exception('Failed to delete blog');
       }
     }
     catch (e) {
--- lib/widget/blog/blog_card.dart
+++ lib/widget/blog/blog_card.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 
 import 'package:blog_app/data/blog.dart';
+import 'package:blog_app/service/blog_service.dart';
 import 'package:flutter/material.dart';
 
 class BlogCard extends StatelessWidget {
@@ -11,7 +12,8 @@ class BlogCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
 
-    return Wrap(
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         blog.headerImage != null ?
           Image.memory(base64Decode(blog.headerImage!)) :
@@ -28,12 +30,73 @@ class BlogCard extends StatelessWidget {
               ),
               Padding(
                 padding: const EdgeInsets.only(top: 16.0),
-                child: Icon( blog.isLikedByMe ? Icons.favorite : Icons.favorite_border, color: const Color( 0xFFC5264E )),
-              )
+                child: Wrap(
+                  children: [
+                    Row(
+                      children: [
+                        IconButton(
+                            icon: blog.isLikedByMe ? const Icon( Icons.favorite ) : const Icon( Icons.favorite_border ),
+                            color: const Color( 0xFFC5264E ),
+                            constraints: const BoxConstraints(),
+                            style: ButtonStyle(
+                                padding: MaterialStateProperty.all(EdgeInsets.zero)
+                            ),
+                            onPressed: () => {}
+                        ),
+                        const Spacer(flex: 2),
+                        IconButton(
+                            icon: const Icon( Icons.edit ),
+                            onPressed: () => {}
+                        ),
+                        IconButton(
+                          icon: const Icon(Icons.delete),
+                          onPressed: () async {
+                            confirmDeletionDialog(context);
+                            // todo: refresh blog list
+                          },
+                        ),
+                      ],
+                    ),
+                  ]
+                ),
+              ),
             ],
           ),
         ),
       ],
+    );
+  }
+  
+  Future<void> confirmDeletionDialog( BuildContext context ) {
+    return showDialog(
+        context: context,
+        builder: (BuildContext context) => AlertDialog(
+          title: const Text("Delete blog"),
+          content: const Text("Are you sure you want to delete this blog?"),
+          actions: [
+            TextButton(
+              onPressed: () => Navigator.of(context).pop(),
+              child: const Text("Cancel"),
+            ),
+            TextButton(
+              onPressed: () async {
+
+                var success = await BlogService.instance.deleteBlog(blog.id!);
+
+                ScaffoldMessenger.of(context).showSnackBar(
+                  SnackBar(
+                    content: Text(success ? "Blog deleted" : "Error while deleting blog"),
+                    backgroundColor: success ? Colors.green : Colors.red,
+                    duration: const Duration(seconds: 2),
+                  ),
+                );
+
+                Navigator.of(context).pop();
+              },
+              child: const Text("Delete"),
+            ),
+          ],
+        )
     );
   }
 }
